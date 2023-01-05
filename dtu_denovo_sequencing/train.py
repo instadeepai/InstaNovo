73e7f5070b0a894d3620c0d969b2a546b3cc6477
Kevin Eloff <k.eloff@hotmail.co.za>
2023-01-05 12:28:19 +0200

    exp: fix validation bias term

--- dtu_denovo_sequencing/train.py
+++ dtu_denovo_sequencing/train.py
@@ -424,7 +424,7 @@ def train(rank: int, cfg: TrainConfig, deepspeed_cfg: argparse.Namespace) -> Non
 
                             bias = x[:, :, 0]
                             x = model.input_embed(x)
-                            x = model.transformer.encoder(x, src_key_padding_mask=x_pad)
+                            x = model.transformer.encoder(x, bias=bias, src_key_padding_mask=x_pad)
 
                             for _ in range(cfg.model_cfg.max_len):
                                 # y_hat = model(x.float(), x_pad, seq)
@@ -434,7 +434,6 @@ def train(rank: int, cfg: TrainConfig, deepspeed_cfg: argparse.Namespace) -> Non
                                 yy = model.transformer.decoder(
                                     yy,
                                     memory=x,
-                                    bias=bias,
                                     tgt_mask=model.transformer.generate_square_subsequent_mask(
                                         seq.shape[1]
                                     ).to(x.device),
