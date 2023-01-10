8997f7947e40aaa71c68d3be8fecf0befcc2c042
Kevin Eloff <k.eloff@hotmail.co.za>
2023-01-10 11:44:12 +0200

    exp: fix positional encoding

--- dtu_denovo_sequencing/model.py
+++ dtu_denovo_sequencing/model.py
@@ -39,8 +39,7 @@ class TransNovo(nn.Module):
     def forward(self, x: Tensor, x_pad: Tensor, y: Tensor, y_pad: Tensor | None = None) -> Tensor:
         """Defines the computation performed at every call."""
         x = self.input_embed(x)  # /10 is fp16 fix
-        # FiX THIS POSITIONAL ENCODING
-        y = self.pos_enc(self.seq_embed(y).transpose(0, 1)).transpose(0, 1)
+        y = self.pos_enc(self.seq_embed(y))
 
         out = self.transformer(
             x,
@@ -71,19 +70,19 @@ class PositionalEncoding(nn.Module):
 
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        pe = torch.zeros(max_len, 1, d_model)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        pe = torch.zeros(1, max_len, d_model)
+        pe[0, :, 0::2] = torch.sin(position * div_term)
+        pe[0, :, 1::2] = torch.cos(position * div_term)
         self.register_buffer("pe", pe)
 
     def forward(self, x: Tensor) -> Tensor:
         """Defines the computation performed at every call.
 
         Args:
-            x (Tensor): shape [seq_len, batch_size, embedding_dim]
+            x (Tensor): shape [batch_size, seq_len, embedding_dim]
 
         Returns:
             Tensor
         """
-        x = x + self.pe[: x.size(0)]
+        x = x + self.pe[:, : x.size(1)]
         return self.dropout(x)
