1d6b76e4cefde15e51d9ac059b021c9aa6342a3b
Jeroen Van Goey <j.vangoey@instadeep.com>
2023-09-01 18:14:30 +0200

    build: add long_description

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from codecs import open
 
 import setuptools
 from setuptools import setup
+from pathlib import Path
 
 __version__ = "0.1.1"
 
@@ -11,6 +12,10 @@ __version__ = "0.1.1"
 def read_requirements(file: str) -> list[str]:
     """Returns content of given requirements file."""
     return [line for line in open(file) if not (line.startswith("#") or line.startswith("--"))]
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 
 setup(
@@ -24,4 +29,6 @@ setup(
     install_requires=read_requirements("./requirements.txt"),
     # extras_require={"dev": read_requirements("./requirements-dev.txt")},
     include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
