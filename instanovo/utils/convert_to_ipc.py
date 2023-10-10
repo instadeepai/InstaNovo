57f1a497c349c055863ec282f5b9631150931cce
Kevin <k.eloff@instadeep.com>
2023-10-10 12:08:32 +0200

    feat: use matchms for mgf

--- instanovo/utils/convert_to_ipc.py
+++ instanovo/utils/convert_to_ipc.py
@@ -13,68 +13,118 @@ import numpy as np
 import pandas as pd
 import polars as pl
 import pyopenms
-from depthcharge.data.hdf5 import AnnotatedSpectrumIndex
+from matchms.importing import load_from_mgf
 from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def convert_mgf_ipc(source: str, target: str, max_charge: int = 10) -> pl.DataFrame:
+def convert_mgf_ipc(
+    source: Path,
+    target: Path,
+    max_charge: int = 10,
+    use_old_schema: bool = False,
+    verbose: bool = True,
+) -> pl.DataFrame:
     """Convert .mgf file to Polars .ipc."""
-    if not Path(source).name.endswith(".mgf"):
-        raise ValueError(
-            f"Attempted to use MGF mode with a non-mgf file: '{source}'. Please specify with --source_type"
-        )
-
-    tmp_dir = tempfile.TemporaryDirectory()
-    idx_fn = os.path.join(tmp_dir.name, f"{uuid.uuid4().hex}.hdf5")
-    valid_charge = np.arange(1, max_charge + 1)
-    index = AnnotatedSpectrumIndex(idx_fn, source, valid_charge=valid_charge)
-
-    logger.info(f"Loaded {index.n_spectra} spectra, converting...")
-
-    data_dict: dict[str, Any] = {
-        "Sequence": [],
-        "Modified sequence": [],
-        "MS/MS m/z": [],
-        "Charge": [],
-        "Mass values": [],
-        "Intensity": [],
+    schema = {
+        "experiment_name": str,
+        "evidence_index": int,
+        "scan_number": int,
+        "sequence": str,
+        "modified_sequence": str,
+        "precursor_mass": float,
+        "precursor_mz": pl.Float64,
+        "precursor_charge": int,
+        "retention_time": pl.Float64,
+        "mz_array": pl.List(pl.Float64),
+        "intensity_array": pl.List(pl.Float64),
     }
 
-    for i in tqdm(range(index.n_spectra), total=index.n_spectra):
-        (
-            mz_array,
-            int_array,
-            precursor_mz,
-            precursor_charge,
-            peptide,
-        ) = index[i]
+    if use_old_schema:
+        schema = {
+            "experiment_name": str,
+            "evidence_index": int,
+            "scan_number": int,
+            "Sequence": str,
+            "Modified sequence": str,
+            "Mass": float,
+            "MS/MS m/z": pl.Float64,
+            "Charge": int,
+            "retention_time": pl.Float64,
+            "Mass values": pl.List(pl.Float64),
+            "Intensity": pl.List(pl.Float64),
+        }
 
-        data_dict["Sequence"].append(peptide)
-        data_dict["Modified sequence"].append(f"_{peptide}_")
-        data_dict["MS/MS m/z"].append(precursor_mz)
-        data_dict["Charge"].append(precursor_charge)
-        data_dict["Mass values"].append(mz_array)
-        data_dict["Intensity"].append(int_array)
+    df = pl.DataFrame(schema=schema)
 
-    df = pl.DataFrame(data_dict)
+    if source.is_file():
+        filenames = [source]
+    else:
+        filenames = source.iterdir()
+
+    for filepath in filenames:
+        if not filepath.suffix.endswith("mgf"):
+            logger.info(f"Skipping {filepath}... Not a mgf file...")
+            continue
+
+        if verbose:
+            logger.info(f"Processing {filepath}...")
+
+        if not filepath.is_file():
+            if verbose:
+                logger.warning("File not found, skipping...")
+            continue
+
+        exp = load_from_mgf(str(filepath))
+
+        data = []
+        evidence_index = 0
+        for spectrum in exp:
+            meta = spectrum.metadata
+
+            peptide = ""
+            if "peptide_sequence" in meta:
+                peptide = meta["peptide_sequence"]
+                unmod_peptide = "".join([x[0] for x in re.split(r"(?<=.)(?=[A-Z])", peptide)])
+
+            if meta["charge"] > max_charge:
+                continue
+
+            scan_id = int(re.findall(r":(\d+)", meta["scans"])[-1])
+
+            data.append(
+                [
+                    Path(meta["title"].replace("\\", "/")).stem,
+                    evidence_index,
+                    scan_id,
+                    unmod_peptide,
+                    peptide if not use_old_schema else f"_{peptide}_",
+                    meta["pepmass"][0],
+                    meta["precursor_mz"],
+                    meta["charge"],
+                    meta["retention_time"],
+                    list(spectrum.mz),
+                    list(spectrum.intensities),
+                ]
+            )
+
+            evidence_index += 1
+        df = pl.concat([df, pl.DataFrame(data, schema=schema)])
 
     Path(target).parent.mkdir(parents=True, exist_ok=True)
     df.write_ipc(target)
-
-    tmp_dir.cleanup()
 
     return df
 
 
 # flake8: noqa: CR001
 def convert_mzml_ipc(
-    source: str,
-    target: str,
+    source: Path,
+    target: Path,
     max_charge: int = 10,
-    use_old_schema: bool = True,
+    use_old_schema: bool = False,
     verbose: bool = True,
 ) -> None:
     """Convert mzml to polars ipc."""
@@ -111,14 +161,16 @@ def convert_mzml_ipc(
 
     df = pl.DataFrame(schema=schema)
 
-    source = Path(source)
-
     if source.is_file():
         filenames = [source]
     else:
         filenames = source.iterdir()
 
     for filepath in filenames:
+        if not filepath.suffix.endswith("mzml"):
+            logger.info(f"Skipping {filepath}... Not a mzml file...")
+            continue
+
         if verbose:
             logger.info(f"Processing {filepath}...")
 
@@ -181,7 +233,7 @@ def main() -> None:
     parser.add_argument("source", help="source file or folder")
     parser.add_argument("target", help="target ipc file to be saved")
     parser.add_argument(
-        "--source_type", default="mgf", choices=["mgf", "mzml", "csv"], help="type of input data"
+        "--source_type", default=None, choices=["mgf", "mzml", "csv"], help="type of input data"
     )
     parser.add_argument("--max_charge", default=10, help="maximum charge to filter out")
     parser.add_argument("--verbose", action="store_true")
@@ -189,17 +241,27 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    source = args.source
-    target = args.target
+    source = Path(args.source)
+    target = Path(args.target)
+    source_type = args.source_type.lower()
 
-    if args.source_type == "mgf":
-        convert_mgf_ipc(source, target, args.max_charge)
-    elif args.source_type == "csv":
-        df = pd.read_csv(source)
-        df = pl.from_pandas(df)
-        Path(target).parent.mkdir(parents=True, exist_ok=True)
-        df.write_ipc(target)
-    elif args.source_type == "mzml":
+    if source_type is None:
+        # Attempt to infer type from file
+        if source.is_dir():
+            raise ValueError(
+                f"Cannot infer source type from a directory. Please specify with --source_type"
+            )
+        source_type = source.suffix[1:].lower()
+
+    if source_type == "mgf":
+        convert_mgf_ipc(
+            source,
+            target,
+            args.max_charge,
+            use_old_schema=args.use_old_schema,
+            verbose=args.verbose,
+        )
+    elif source_type == "mzml":
         convert_mzml_ipc(
             source,
             target,
@@ -207,8 +269,13 @@ def main() -> None:
             use_old_schema=args.use_old_schema,
             verbose=args.verbose,
         )
+    elif source_type == "csv":
+        df = pd.read_csv(source)
+        df = pl.from_pandas(df)
+        Path(target).parent.mkdir(parents=True, exist_ok=True)
+        df.write_ipc(target)
     else:
-        assert Exception(f"Source type {args.source_type} not supported.")
+        raise ValueError(f"Source type {source_type} not supported.")
 
 
 if __name__ == "__main__":
